372f8b20e56f2be6336c4dfa1d7cec1af425e997
sorakawa101 <s2230063@edu.cc.uec.ac.jp>
2023-01-03 22:01:34 +0900

    add: fontawesomeのアイコンを使ってナビゲーションバー完成

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 import "./Navbar.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHouse, faFilePen, faArrowRightToBracket } from '@fortawesome/free-solid-svg-icons'
 
 const Navbar = () => {
     return (
         <nav>
-            <Link to="/">Home</Link>
-            <Link to="/createpost">CreatePost</Link>
-            <Link to="/login">Login</Link>
+            <Link to="/"><FontAwesomeIcon icon={faHouse} />Home</Link>
+            <Link to="/createpost"><FontAwesomeIcon icon={faFilePen} />CreatePost</Link>
+            <Link to="/login"><FontAwesomeIcon icon={faArrowRightToBracket} />Login</Link>
         </nav>
     )
 }
