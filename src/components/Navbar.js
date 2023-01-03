a8e45c5751a72e5d77eb0caf7f416b41208897dd
sorakawa101 <s2230063@edu.cc.uec.ac.jp>
2023-01-03 21:47:37 +0900

    add: ナビゲーションバーを追加してCSSを適用

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+
+const Navbar = () => {
+    return (
+        <nav>
+            <Link to="/">Home</Link>
+            <Link to="/createpost">CreatePost</Link>
+            <Link to="/login">Login</Link>
+        </nav>
+    )
+}
+
+export default Navbar
