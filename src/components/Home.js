5151ef1f40df8c4b401a8d0ee3565b93cf23fd35
sorakawa101 <s2230063@edu.cc.uec.ac.jp>
2023-01-21 11:34:55 +0900

    add: Homeコンポーネントを作成＆CSS適用

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import "./Home.css";
 
 const Home = () => {
     return (
-        <div>Home</div>
+        <div className='homePage'>
+                <div className="postContents">
+                    <div className="postHeader">
+                        <h1>タイトル</h1>
+                    </div>
+                <div className="postTextContainer">
+                    今はReactの学習中です。これから頑張ってReactエンジニアとして活躍していきたいと思っています。よろしくお願いします。
+                </div>
+                <div className="nameAndDeleteButton">
+                    <h3>@ShinCode</h3>
+                    <button>削除</button>
+                </div>
+            </div>
+        </div>
     )
 }
 
